e19d805d960720e453888f2d485bf3c1df5100e3
Dan Coughlin <dan.coughlin@gmail.com>
2015-03-24 08:54:23 -0400

    added votes

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,3 @@
 class Topic < ActiveRecord::Base
+  has_many :votes, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223223530) do
+ActiveRecord::Schema.define(version: 20150320011650) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
