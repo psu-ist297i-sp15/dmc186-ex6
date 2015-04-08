c25cec4c43e2304088f0293e42f3c5c836461600
Dan Coughlin <dan.coughlin@gmail.com>
2015-04-07 20:53:38 -0400

    adding controllers and seeded database.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :topics do
     member do
       post 'upvote'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do |i|
+  t = Topic.create(title: "Topic ##{i}", description: "This topic is cool because everyone loves the topic number ##{i}.")
+  rand(1..100).times do |j|
+    t.votes.create
+  end
+end
+x = User.new
+x.email='dan.coughlin@gmail.com'
+x.password='12345678'
+x.password_confirmation='12345678'
+x.save!
